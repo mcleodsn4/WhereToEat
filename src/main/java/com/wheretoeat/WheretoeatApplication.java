77387477e9300e12f624a4564aac3929128ff46f
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-17 07:21:16 -0400

    initial commit of Spring Boot Project

---
+++ src/main/java/com/wheretoeat/WheretoeatApplication.java
@@ -0,0 +1,13 @@
+package com.wheretoeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheretoeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheretoeatApplication.class, args);
+	}
+
+}
