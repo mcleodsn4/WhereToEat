523ed252d0e9dc793421edabf5be68760b9b837d
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-17 21:54:01 -0400

    controller

--- src/main/java/com/wheretoeat/WhereToEatController.java
+++ src/main/java/com/wheretoeat/WhereToEatController.java
@@ -1,5 +1,14 @@
 package com.wheretoeat;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class WhereToEatController {
 
+	@RequestMapping("/start")
+	public String start() {
+		return "start";
+	}
+	
 }
