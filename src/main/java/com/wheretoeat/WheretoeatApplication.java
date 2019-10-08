e58dedd7705c5b5fbcb003a6ada51308c5e0012e
Joel Vincent <imjoelvincent@gmail.com>
2019-10-08 15:10:44 -0400

    added static folder, updated pom, and controller to include css, js, and images

--- src/main/java/com/wheretoeat/WheretoeatApplication.java
+++ src/main/java/com/wheretoeat/WheretoeatApplication.java
@@ -1,13 +1,36 @@
 package com.wheretoeat;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @SpringBootApplication
-public class WheretoeatApplication {
+@Configuration
+@EnableWebMvc
+public class WheretoeatApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WheretoeatApplication.class, args);
 	}
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/webjars/**",
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/META-INF/resources/webjars/",
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+	
 }
