e8349ebd7a1a877022ad4fdad0469d0baed7d236
lopezrjo <lopezrjo@JUANOLOPEZR7CDE.ad.uc.edu>
2019-10-11 05:06:37 -0400

    RestaurantDTO created - REstaurant class and enum for Food type

---
+++ src/main/java/com/wheretoeat/dto/FoodType.java
@@ -0,0 +1,24 @@
+package com.wheretoeat.dto;
+
+public enum FoodType {
+	
+	ASIAN("Asian"),
+	CAFE("Cafe"),
+	CHINESE("Chinese"),
+	FASTFOOD("Fast-Food"),
+	INDIAN("Indian"),
+	ITALIAN("Italian"),
+	PIZZA("Pizza"),
+	VEGETARIAN("Vegetarian");
+	
+	private String type;
+	
+	FoodType(String type){
+		this.type = type;
+	}
+	
+	public String type() {
+		return type;
+	}
+	
+}
