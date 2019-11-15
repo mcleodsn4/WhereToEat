79c1aff906697f048988d97da283cc7d4fc1af42
vuhi <vuhi@mail.uc.edu>
2019-11-14 22:07:33 -0500

    add exception

--- src/main/java/com/wheretoeat/service/IRestaurantService.java
+++ src/main/java/com/wheretoeat/service/IRestaurantService.java
@@ -11,11 +11,11 @@ public interface IRestaurantService {
 	 * @param type - Takes a food type (category) enum 
 	 * @return will return a restaurant object based on the food type selected.
 	 */
-	RestaurantDTO fetchByFoodType(FoodType type);
+	RestaurantDTO fetchByFoodType(FoodType type) throws Exception;
 
 	/**
 	 * This class should fetch a random restaurant. Will most like return a value from a 
 	 * @return
 	 */
-	RestaurantDTO fetchRandomRestaurant();
+	RestaurantDTO fetchRandomRestaurant() throws Exception;
 }
